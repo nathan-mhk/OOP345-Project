7deb50f2eae33fa02d72618cfa8488c7c36bc40e
Nathan Kong <mhk.nathan@gmail.com>
2024-07-04 16:24:14 -0400

    [MS2] Init

---
+++ MS2/CustomerOrder.cpp
@@ -0,0 +1,9 @@
+// Name: Nathan Kong
+// Seneca Student ID: 150950236
+// Seneca email: nkong@myseneca.ca
+// Date of completion: 2024-07-0
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#include "CustomerOrder.h"
---
+++ MS2/CustomerOrder.h
@@ -0,0 +1,16 @@
+// Name: Nathan Kong
+// Seneca Student ID: 150950236
+// Seneca email: nkong@myseneca.ca
+// Date of completion: 2024-07-0
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SENECA_CUSTOMERORDER_H
+#define SENECA_CUSTOMERORDER_H
+
+namespace seneca {
+    class CustomerOrder {};
+}
+
+#endif
